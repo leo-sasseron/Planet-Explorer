4fa5cb27569578823422400a4e27390bb886c0b6
Leonardo Sasseron <leo.dias360@gmail.com>
2019-04-06 19:39:22 -0300

    First

---
+++ Planets/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public Navigation nav;
+	public GameObject[] listaPlanetas;
+
+	void Update() {
+
+		if (Input.anyKeyDown) {
+			
+			try {
+				int i = int.Parse (Input.inputString);
+
+				if (i > 0 && i <= 9)
+					nav.PlanetToGo = listaPlanetas [i - 1];
+			}
+
+			catch {
+
+			}
+		
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha0)) {
+
+			nav.PlanetToGo = null;
+		}
+	}
+}
